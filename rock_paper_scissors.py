d1df79de05c8f40cb36219c68453da4c180237d5
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-04-08 19:37:48 +0530

    completed program

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,9 +2,32 @@ import random
 
 def play():
     print("Welcome, lets play rock paper scissors with computer!")
-    user = input("ENTER YOUR CHOICE:'r' for rock,'p' for paper and 's' for scissors:")
     choice = ['r','p','s']
-    cp = random.choice(choice)
     score_u = 0
     score_c = 0
-    
+    round = 1
+    while round <= 3:
+        print(f"Round {round}")
+        user = input("ENTER YOUR CHOICE:'r' for rock,'p' for paper and 's' for scissors:")
+        cp = random.choice(choice)
+        if isWin(user,cp):
+            score_u=score_u+1
+            print('User wins this round!')
+            print(f"Current points are: User = {score_u} and Computer = {score_c}")
+        else:
+            score_c=score_c+1
+            print('Computer wins this round!')
+            print(f"Current points are: User = {score_u} and Computer = {score_c}")
+        round=round+1
+    if score_c < score_u:
+        print("User wins the Game!")
+    else:
+        print("Hard Luck, Try again next time!")
+
+def isWin(u, cp):
+    if (u=='r' and cp=='s') or (u=='p' and cp == 'r') or (u=='s' and cp =='p'):
+        return True
+    else:
+        return False
+
+play()
